90fdfea895b03148b28bcaae6127a100b03e70bc
shnarda <shnarda@outlook.com>
2024-12-18 19:43:01 +0300

    Documentation is updated

--- docs/html/search/defines_2.js
+++ docs/html/search/defines_2.js
@@ -6,7 +6,11 @@ var searchData=
   ['taggedobjectmacro_5finitialize_5fmember_5funpack_3',['TAGGEDOBJECTMACRO_INITIALIZE_MEMBER_UNPACK',['../taggedjsonobjectmacros_8h.html#a8b5ccd1e8c837ac08ba6dd4075e6fec2',1,'taggedjsonobjectmacros.h']]],
   ['taggedobjectmacro_5finitialize_5fmember_5fvalue_4',['TAGGEDOBJECTMACRO_INITIALIZE_MEMBER_VALUE',['../taggedjsonobjectmacros_8h.html#a22b644eea40f903cea4eec35f7a4cd9f',1,'taggedjsonobjectmacros.h']]],
   ['taggedobjectmacro_5finitialize_5fmember_5fvalue_5funpack_5',['TAGGEDOBJECTMACRO_INITIALIZE_MEMBER_VALUE_UNPACK',['../taggedjsonobjectmacros_8h.html#a667f62ccaae32bced27b8c72ce01950b',1,'taggedjsonobjectmacros.h']]],
-  ['tjo_5fdefine_5fjson_5ftagged_5fobject_6',['TJO_DEFINE_JSON_TAGGED_OBJECT',['../taggedjsonobjectmacros_8h.html#a08d8e60f158feaf54caefec7348efc14',1,'taggedjsonobjectmacros.h']]],
-  ['tjo_5fis_5ftagged_5fobject_7',['TJO_IS_TAGGED_OBJECT',['../taggedjsonarray_8h.html#a9c40f81ba0062308d1e8f74937b5fc13',1,'taggedjsonarray.h']]],
-  ['tjo_5fjson_5fcompatible_8',['TJO_JSON_COMPATIBLE',['../taggedjsonarray_8h.html#ae65d5337ba37227e11264253f5abafc6',1,'TJO_JSON_COMPATIBLE:&#160;taggedjsonarray.h'],['../taggedjsonobject_8h.html#ae65d5337ba37227e11264253f5abafc6',1,'TJO_JSON_COMPATIBLE:&#160;taggedjsonobject.h']]]
+  ['taggedobjectmacro_5flist_5fmembers_6',['TAGGEDOBJECTMACRO_LIST_MEMBERS',['../taggedjsonobjectmacros_8h.html#aa44cf414274649db1113b8b556c2e795',1,'taggedjsonobjectmacros.h']]],
+  ['taggedobjectmacro_5flist_5fmembers_5funpack_7',['TAGGEDOBJECTMACRO_LIST_MEMBERS_UNPACK',['../taggedjsonobjectmacros_8h.html#af5f4072337c4eb25442ce3329b7267d2',1,'taggedjsonobjectmacros.h']]],
+  ['taggedobjectmacro_5fmove_5fparameters_8',['TAGGEDOBJECTMACRO_MOVE_PARAMETERS',['../taggedjsonobjectmacros_8h.html#ae067cadfd3acaae017dd6669f99e24e7',1,'taggedjsonobjectmacros.h']]],
+  ['taggedobjectmacro_5fmove_5fparameters_5funpack_9',['TAGGEDOBJECTMACRO_MOVE_PARAMETERS_UNPACK',['../taggedjsonobjectmacros_8h.html#afe582aa50676acb7394ab3ea875f85c4',1,'taggedjsonobjectmacros.h']]],
+  ['tjo_5fdefine_5fjson_5ftagged_5fobject_10',['TJO_DEFINE_JSON_TAGGED_OBJECT',['../taggedjsonobjectmacros_8h.html#a08d8e60f158feaf54caefec7348efc14',1,'taggedjsonobjectmacros.h']]],
+  ['tjo_5fis_5ftagged_5fobject_11',['TJO_IS_TAGGED_OBJECT',['../taggedjsonarray_8h.html#a9c40f81ba0062308d1e8f74937b5fc13',1,'taggedjsonarray.h']]],
+  ['tjo_5fjson_5fcompatible_12',['TJO_JSON_COMPATIBLE',['../taggedjsonarray_8h.html#ae65d5337ba37227e11264253f5abafc6',1,'TJO_JSON_COMPATIBLE:&#160;taggedjsonarray.h'],['../taggedjsonobject_8h.html#ae65d5337ba37227e11264253f5abafc6',1,'TJO_JSON_COMPATIBLE:&#160;taggedjsonobject.h']]]
 ];
