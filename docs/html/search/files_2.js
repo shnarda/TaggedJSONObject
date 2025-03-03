441efef41d1ec1b58afbac6a7dc175195a052aaf
shnarda <shnarda@outlook.com>
2025-03-03 19:08:03 +0300

    Documentation is added for the new features

---
+++ docs/html/search/files_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['taggedjsonarray_2eh_85',['taggedjsonarray.h',['../taggedjsonarray_8h.html',1,'']]],
+  ['taggedjsonobject_2eh_86',['taggedjsonobject.h',['../taggedjsonobject_8h.html',1,'']]],
+  ['taggedjsonobjectmacros_2eh_87',['taggedjsonobjectmacros.h',['../taggedjsonobjectmacros_8h.html',1,'']]]
+];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['taggedjsonobject_159',['TaggedJSONObject',['../index.html',1,'']]]
+];
